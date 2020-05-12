3b2dccf05f948eb20e8b39a3b97b967a44cece9b
wandeltk <wandeltk@gmail.com>
2020-05-12 08:46:33 -0600

    make HTML insert

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,32 @@
-/*
-function addRow(table: string){
-    let tableRef = document.getElementById(table);
-    let newRow = tableRef.insertRow(-1);
-    let newCell = newRow.insertCell(0);
-    let newText = document.
-}
-*/
 
-async function getAllUsers(url:string){
+function dataTable(table: string){
+    
+    document.getElementById(table).innerHTML = 
+    
+    `<table class="table table-hover">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Username</th>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Role</th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>`
+}
+
+
+async function getFunction(url:string){
     const response = await fetch(url);
     const json = response.json();
     console.log(json);
     return(json);
 }
 
-console.log(getAllUsers("http://project1api-env.eba-2v5prypq.us-east-1.elasticbeanstalk.com/users"))
+//console.log(getFunction("http://project1api-env.eba-2v5prypq.us-east-1.elasticbeanstalk.com/users"))
+
+dataTable('user-table');
