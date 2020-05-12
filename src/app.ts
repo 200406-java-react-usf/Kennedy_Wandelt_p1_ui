9480651d8b32f06b41f17d525793ef368285b405
wandeltk <wandeltk@gmail.com>
2020-05-12 08:30:22 -0600

    fixed getAllUsers method

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
+/*
 function addRow(table: string){
     let tableRef = document.getElementById(table);
     let newRow = tableRef.insertRow(-1);
     let newCell = newRow.insertCell(0);
     let newText = document.
 }
+*/
 
 async function getAllUsers(url:string){
     const response = await fetch(url);
     const json = response.json();
     console.log(json);
+    return(json);
 }
 
-let link = 'http://project1api-env.eba-2v5prypq.us-east-1.elasticbeanstalk.com/users';
-
-console.log(getAllUsers(link))
+console.log(getAllUsers("http://project1api-env.eba-2v5prypq.us-east-1.elasticbeanstalk.com/users"))
