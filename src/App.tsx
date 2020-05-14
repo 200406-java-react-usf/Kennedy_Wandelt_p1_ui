c4b4fa48a3878e5d8ccc76b7fa000d09ac47da34
wandeltk <wandeltk@gmail.com>
2020-05-13 21:00:41 -0600

    added reimb  model

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import { LoginComponent } from './components/LoginComp';
-import { Home } from './components/Home';
-import { UserTable } from './components/Users'
+import { LoginComponent } from './components/LoginComponents';
+import { Home } from './components/NavbarComponent';
+import { UserTable } from './components/UserTableComponent'
 
 function App() {
   return (
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,10 +1,20 @@
-export class User{
-    username: string;
-    password: string;
+export class User {
 
-    constructor(un: string, pw: string){
-        this.username = un;
-        this.password = pw;
+    id: number | null;
+    un: string;
+    pass: string;
+    fn: string;
+    ln: string;
+    email: string;
+    role: string;
+
+    constructor (un: string, pass: string, fn: string, ln: string, email: string, role: string){
+        this.id = null;
+        this.un = un;
+        this.pass = pass;
+        this.fn = fn;
+        this.ln = ln;
+        this.email = email;
+        this.role = role; 
     }
-    
 }
