ec8467d0e9a7a51b06308aba3482e3c13259801c
wandeltk <wandeltk@gmail.com>
2020-05-14 22:55:32 -0600

    made navbar ilnks conditional

--- src/components/NavbarComponent.tsx
+++ src/components/NavbarComponent.tsx
@@ -22,40 +22,61 @@ const NavbarComponent = (props: INavbarProps) => {
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                    <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <a className="nav-link" href="/my-reimbs">My Reimbursements</a>
-                    </li>
-                    <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Admin Dashboard
-                        </a>
-                        <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a className="dropdown-item" href="#">Action</a>
-                        <a className="dropdown-item" href="#">Another action</a>
-                        <a className="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </li>
-                    <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Financial Dashboard
-                        </a>
-                        <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a className="dropdown-item" href="#">Action</a>
-                        <a className="dropdown-item" href="#">Another action</a>
-                        <a className="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </li>
-                    </ul>
-                </div>
-                <div className="navbar-collapse">
-                    <ul className="navbar-nav ml-auto">
+                    <ul className="navbar-nav">                  
+                    {props.authUser ?  
                         <li className="nav-item">
-                            <a className="nav-link" href="/login">Login</a>
-                        </li>
+                            <a className="nav-link" href="/my-reimbs">My Reimbursements</a>
+                        </li>:
 
+                        <></>
+                    }
+                    {(props.authUser && props.authUser.role === 'admin')?
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Admin Dashboard
+                            </a>
+                            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a className="dropdown-item" href="#">Action</a>
+                            <a className="dropdown-item" href="#">Another action</a>
+                            <a className="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </li>:
+                        <></>
+                    }
+                    {(props.authUser && props.authUser.role === 'fmanager')?
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Financial Dashboard
+                            </a>
+                            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a className="dropdown-item" href="#">Action</a>
+                            <a className="dropdown-item" href="#">Another action</a>
+                            <a className="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </li>:
+                        <></>
+                    }
                     </ul>
                 </div>
+                <>
+                {!props.authUser ?
+                    <div className="navbar-collapse">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <a className="nav-link" href="/login">Login</a>
+                            </li>
+                        </ul>
+                    </div>:
+
+                     <div className="navbar-collapse">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <a className="nav-link" href="/login">Logout</a>
+                            </li>
+                        </ul>
+                    </div>
+                }
+                </>
             </nav>
         </>
     );
