13a7b0df4b98a11b9ccbe784bb2f9f3a7d76f608
wandeltk <wandeltk@gmail.com>
2020-05-16 15:10:56 -0600

    changed baseURL to connect to elastic beanstalk

--- src/remote/revaboards-client.ts
+++ src/remote/revaboards-client.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const projectClient = axios.create({
-    baseURL: 'http://localhost:8080',
-    //baseURL: 'http://project1api-env.eba-2v5prypq.us-east-1.elasticbeanstalk.com',
+    //baseURL: 'http://localhost:8080',
+    baseURL: 'http://project1api-env.eba-2v5prypq.us-east-1.elasticbeanstalk.com',
     headers: {
         'Content-Type': 'application/json'
     }
